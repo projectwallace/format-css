b2c17b119a4abeacd7b3cc259916abd171e46723
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2024-02-17 16:44:24 +0100

    additional tests for combinators and exotic syntax (#35)
    closes #34 
    closes #27

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
-import { suite } from "uvu";
-import * as assert from "uvu/assert";
-import { format } from "../index.js";
+import { suite } from "uvu"
+import * as assert from "uvu/assert"
+import { format } from "../index.js"
 
-let test = suite("Stylesheet");
+let test = suite("Stylesheet")
 
 test("empty input", () => {
-	let actual = format(``);
-	let expected = ``;
-	assert.equal(actual, expected);
+	let actual = format(``)
+	let expected = ``
+	assert.equal(actual, expected)
 });
 
 test('handles invalid input', () => {
@@ -17,4 +17,47 @@ test('handles invalid input', () => {
 	assert.equal(actual, expected)
 })
 
-test.run();
+test('Vadim Makeevs example works', () => {
+	let actual = format(`
+	@layer what {
+		@container (width > 0) {
+			ul:has(:nth-child(1 of li)) {
+				@media (height > 0) {
+					&:hover {
+						--is: this;
+					}
+				}
+			}
+		}
+	}
+	`)
+	let expected = `@layer what {
+	@container (width > 0) {
+		ul:has(:nth-child(1 of li)) {
+			@media (height > 0) {
+				&:hover {
+					--is: this;
+				}
+			}
+		}
+	}
+}`
+	assert.equal(actual, expected)
+})
+
+test('minified Vadims example', () => {
+	let actual = format(`@layer what{@container (width>0){@media (min-height:.001px){ul:has(:nth-child(1 of li)):hover{--is:this}}}}`)
+
+	let expected = `@layer what {
+	@container (width > 0) {
+		@media (min-height: .001px) {
+			ul:has(:nth-child(1 of li)):hover {
+				--is: this;
+			}
+		}
+	}
+}`
+	assert.equal(actual, expected)
+})
+
+test.run()
