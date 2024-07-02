001e3ee1051a6a5b25b00c15721923e84a77ce41
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2024-07-02 20:29:55 +0200

    Add benchmarks and integrate CodSpeed (#71)

---
+++ benchmarks/benchmarks.js
@@ -0,0 +1,18 @@
+import { Bench } from "tinybench";
+import { withCodSpeed } from "@codspeed/tinybench-plugin";
+import * as fs from "fs";
+import { format } from '../index.js'
+
+let filelist = fs.readdirSync('./benchmarks/__fixtures__')
+
+let bench = withCodSpeed(new Bench())
+
+for (let filename of filelist) {
+	let css = fs.readFileSync(`./benchmarks/__fixtures__/${filename}`, 'utf8')
+	let byte_size = (Buffer.byteLength(css) / 1024).toFixed(1)
+	bench.add(`${filename} (${byte_size}kB)`, () => format(css))
+}
+
+await bench.run()
+
+console.table(bench.table())
