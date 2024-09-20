212961b645b328823bc736121ad5dc2661eeecf1
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2024-09-20 16:16:00 +0200

    only 2 minifications

--- benchmarks/benchmarks.js
+++ benchmarks/benchmarks.js
@@ -10,8 +10,11 @@ let bench = withCodSpeed(new Bench())
 for (let filename of filelist) {
 	let css = fs.readFileSync(`./benchmarks/__fixtures__/${filename}`, 'utf8')
 	let byte_size = (Buffer.byteLength(css) / 1024).toFixed(1)
-	bench.add(`format ${filename} (${byte_size}kB)`, () => format(css))
-	bench.add(`minify ${filename} (${byte_size}kB)`, () => minify(css))
+	bench.add(`${filename} (${byte_size}kB)`, () => format(css))
+
+	if (filename.includes('nerdy') || filename.includes('vistaprint')) {
+		bench.add(`minify ${filename} (${byte_size}kB)`, () => minify(css))
+	}
 }
 
 await bench.warmup()
