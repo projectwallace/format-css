59c00e758ccfcb8e313d9c01a9379683d4383618
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2026-03-28 23:14:18 +0100

    Chore/skip build before lint (#188)
    closes #187

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -433,7 +433,7 @@ export function format(
 		return prelude
 			.replace(/\s*([:,])/g, prelude.toLowerCase().includes('selector(') ? '$1' : '$1 ') // force whitespace after colon or comma, except inside `selector()`
 			.replace(/\)([a-zA-Z])/g, ') $1') // force whitespace between closing parenthesis and following text (usually and|or)
-			.replace(/\s*(=>|<=)\s*/g, ' $1 ') // force whitespace around => and <=
+			.replace(/\s*(=>|>=|<=)\s*/g, `${OPTIONAL_SPACE}$1${OPTIONAL_SPACE}`) // add optional spacing around =>, >= and <=
 			.replace(/([^<>=\s])([<>])([^<>=\s])/g, `$1${OPTIONAL_SPACE}$2${OPTIONAL_SPACE}$3`) // add spacing around < or > except when it's part of <=, >=, =>
 			.replace(/\s+/g, OPTIONAL_SPACE) // collapse multiple whitespaces into one
 			.replace(
