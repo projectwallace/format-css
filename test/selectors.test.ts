a4243be8767414fab8bf18c469d35d69786d0ce0
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2026-03-30 19:51:42 +0200

    chore: add test for minifying nth selectors (#190)

--- test/selectors.test.ts
+++ test/selectors.test.ts
@@ -117,21 +117,30 @@ d::first-letter {}`
 	expect(actual).toEqual(expected)
 })
 
-test('formats selectors with Nth', () => {
-	let fixtures = [
-		[`li:nth-child(3n-2) {}`, `li:nth-child(3n -2) {}`],
-		[`li:nth-child(0n+1) {}`, `li:nth-child(0n + 1) {}`],
-		[`li:nth-child(even of .noted) {}`, `li:nth-child(even of .noted) {}`],
-		[`li:nth-child(2n of .noted) {}`, `li:nth-child(2n of .noted) {}`],
-		[`li:nth-child(-n + 3 of .noted) {}`, `li:nth-child(-n + 3 of .noted) {}`],
-		[`li:nth-child(-n+3 of li.important) {}`, `li:nth-child(-n + 3 of li.important) {}`],
-		[`p:nth-child(n+8):nth-child(-n+15) {}`, `p:nth-child(n + 8):nth-child(-n + 15) {}`],
-	]
+test.each([
+	[`li:nth-child(3n-2) {}`, `li:nth-child(3n -2) {}`],
+	[`li:nth-child(0n+1) {}`, `li:nth-child(0n + 1) {}`],
+	[`li:nth-child(even of .noted) {}`, `li:nth-child(even of .noted) {}`],
+	[`li:nth-child(2n of .noted) {}`, `li:nth-child(2n of .noted) {}`],
+	[`li:nth-child(-n + 3 of .noted) {}`, `li:nth-child(-n + 3 of .noted) {}`],
+	[`li:nth-child(-n+3 of li.important) {}`, `li:nth-child(-n + 3 of li.important) {}`],
+	[`p:nth-child(n+8):nth-child(-n+15) {}`, `p:nth-child(n + 8):nth-child(-n + 15) {}`],
+])('formats nth selector: %s', (css, expected) => {
+	let actual = format(css)
+	expect(actual).toEqual(expected)
+})
 
-	for (let [css, expected] of fixtures) {
-		let actual = format(css)
-		expect(actual).toEqual(expected)
-	}
+test.each([
+	[`li:nth-child(3n-2) {}`, `li:nth-child(3n-2){}`],
+	[`li:nth-child(0n+1) {}`, `li:nth-child(0n+1){}`],
+	[`li:nth-child(even of .noted) {}`, `li:nth-child(even of .noted){}`],
+	[`li:nth-child(2n of .noted) {}`, `li:nth-child(2n of .noted){}`],
+	[`li:nth-child(-n + 3 of .noted) {}`, `li:nth-child(-n+3 of .noted){}`],
+	[`li:nth-child(-n+3 of li.important) {}`, `li:nth-child(-n+3 of li.important){}`],
+	[`p:nth-child(n+8):nth-child(-n+15) {}`, `p:nth-child(n+8):nth-child(-n+15){}`],
+])('minifies nth selector: %s', (css, expected) => {
+	let actual = format(css, { minify: true })
+	expect(actual).toEqual(expected)
 })
 
 test('formats multiline selectors', () => {
